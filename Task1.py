098fe1480736b866620812591a15d4b8f23ad1b4
unknown <pm68308@gmail.com>
2022-09-23 08:58:07 +0300

    init commit

---
+++ Task1.py
@@ -0,0 +1,11 @@
+meal = input('Прием пищи')
+if meal == 'Зафтрак':
+    print('Каша')
+else:
+    wish = input('Предпочтение')
+    if wish == 'Поесть плотно':
+        print('Плов')
+    else:
+        print('Котлета с пюре')
+
+
