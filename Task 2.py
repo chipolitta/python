fce33d87aa46bec0e3809e6988623a2765afa45f
Polina Makhrova <114076060+chipolitta@users.noreply.github.com>
2022-09-28 15:17:00 +0300

    Delete Task 2.py

--- Task 2.py
+++
@@ -1,8 +0,0 @@
-Total = int(input('Сумма'))
-Time = int(input('Текущий час'))
-if Time >= 10 and Time <=12:
-    Total = Total/2
-    print('Сумма',Total)
-elif Time >=20 and Time <=22:
-    Total = Total/2
-    print('Сумма')
