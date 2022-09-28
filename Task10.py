ec33871786fa710a9d076e68a1cda2a6c1aade78
Polina Makhrova <114076060+chipolitta@users.noreply.github.com>
2022-09-28 15:17:52 +0300

    Delete Task10.py

--- Task10.py
+++
@@ -1,6 +0,0 @@
-string ='+7(991)80-69-162'
-elements = ['(', ')', '-', ' ']
-for i in range(len(elements)):
-    if elements[i] in string:
-        string =string.replace(elements[i], '')
-        print(string)
