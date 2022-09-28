985c8feb139f1ad2459d79522468dbc01331754c
Polina Makhrova <114076060+chipolitta@users.noreply.github.com>
2022-09-28 15:22:17 +0300

    Delete Task4.py

--- Task4.py
+++
@@ -1,8 +0,0 @@
-review =''
-while True:
-    review = input('Введите отзыв:').lower()
-    if review == ' off':
-        print('Сситема предпотений настроена.')
-        break
-    else:
-        print('Спасибо, ваш отзыв принят!')
