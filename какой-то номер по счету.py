92a9d6fdd998a8d3cd1aaafffeeefa5faae884ac
Polina Makhrova <114076060+chipolitta@users.noreply.github.com>
2022-09-28 15:18:15 +0300

    Delete какой-то номер по счету.py

--- какой-то номер по счету.py
+++
@@ -1,8 +0,0 @@
-string = 'Экономический рост тесно связан с ростом общего благосостояния.'
-A = 'ический'
-B = 'ическая'
-if A in string:
-    string = string.replace(A, '.')
-else:
-    string = string.replace(B, '.')
-print(string)
